973b419253f0aecb4a51a94709d1f98e149c92c8
Dmitriy Bronnikov <dbron9269@gmail.com>
2024-03-10 20:37:20 +0200

    feat: implement user endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { readFile } from 'fs/promises';
 import { parse } from 'yaml';
+import { ResponseTransformerInterceptor } from './interceptors/response.transformer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ResponseTransformerInterceptor());
+  app.setGlobalPrefix('api');
+
   const yamlSchema = await readFile('./doc/api.yaml', 'utf-8');
   const apiSchema = parse(yamlSchema);
 
---
+++ src/users/users.service.ts
@@ -0,0 +1,69 @@
+import {
+  Injectable,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
+import { User } from 'src/entities/user';
+import { CreateUserDto } from './dto/create-user.dto';
+import { v4 as uuid4 } from 'uuid';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [];
+
+  getAll(): User[] {
+    return this.users;
+  }
+
+  get(id: string): User | undefined {
+    return this.users.find((user) => {
+      return user.id === id;
+    });
+  }
+
+  create(userDto: CreateUserDto): User {
+    const user: User = new User(
+      uuid4(),
+      userDto.login,
+      userDto.password,
+      1,
+      Date.now(),
+      Date.now(),
+    );
+
+    this.users.push(user);
+    return user;
+  }
+
+  updatePassword(updatePasswordDto: UpdatePasswordDto, userId: string): User {
+    const user = this.users.find((user) => {
+      return user.id === userId;
+    });
+
+    if (user === undefined) {
+      throw new NotFoundException(`User with id ${userId} not found`);
+    }
+
+    if (user.password !== updatePasswordDto.oldPassword) {
+      throw new ForbiddenException();
+    }
+
+    user.password = updatePasswordDto.newPassword;
+    user.updatedAt = Date.now();
+
+    return user;
+  }
+
+  delete(userId: string) {
+    const userIndex = this.users.findIndex((user) => {
+      return user.id === userId;
+    });
+
+    if (userIndex < 0) {
+      throw new NotFoundException(`User with id ${userId} not found`);
+    }
+
+    this.users.splice(userIndex, 1);
+  }
+}
