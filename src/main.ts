d202c31997fb9ce98e9675a877564bb5ea4375c2
Dmitriy Bronnikov <dbron9269@gmail.com>
2024-03-10 16:47:14 +0200

    fix: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const yamlSchema = await readFile('./doc/api.yaml', 'utf-8');
+  const apiSchema = parse(yamlSchema);
+
+  SwaggerModule.setup('api', app, apiSchema);
+
   await app.listen(4000);
 }
 bootstrap();
