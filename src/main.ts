5f496bd68562437d85e4433419b24b47d9aa7ddc
Dmitriy Bronnikov <dbron9269@gmail.com>
2024-03-11 21:10:31 +0200

    fix: remove redundant comment

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ResponseTransformerInterceptor());
-  // app.setGlobalPrefix('api');
 
   const yamlSchema = await readFile('./doc/api.yaml', 'utf-8');
   const apiSchema = parse(yamlSchema);
